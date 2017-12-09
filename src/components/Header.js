1e4aab1af82558850dc4fc29c78f45265c814c18
superzazou (@home) <vincent.e.dumont@gmail.com>
2017-12-09 16:29:31 +0100

    update header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,20 @@ class Header extends Component {
     render() {
         return (
             <div className="header">
-                <Link to="/">
-                    <img src={logo} className="App-logo" alt="logo" />
-                </Link>
-                <h1>MENU NAV</h1>
+                <div className='logo'>
+                    <Link to="/">
+                        <img src={logo} className="App-logo" alt="logo" />
+                    </Link>
+                </div>
+                <div className='menu'>
+                    <div className='menuItem'>Graphic Design</div>
+                    <div className='menuItem'>Motion</div>
+                    <div className='menuItem'>Illustration</div>
+                    <div className='menuItem'>About</div>
+                    <div className='social'>
+                </div>
+                
+                </div>
             </div>
         );
     }
