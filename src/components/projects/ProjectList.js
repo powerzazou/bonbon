a5c039260a9ceaa23c94f684fcb45bf5b45105d4
superzazou (@home) <vincent.e.dumont@gmail.com>
2017-12-09 15:55:15 +0100

    add flexbox display for projects

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import ProjectPreview from './ProjectPreview'
+import './ProjectList.css';
 
 class ProjectList extends Component {
     render() {
         return (
-            <div>
+            <div class='projectList'>
                 <h2>Projets</h2>
-                {window.bonbonProjects.sort((a, b) => {
-                    return a.order - b.order;
-                }).map((project) => {
-                    return <ProjectPreview key={project.id} project={project}></ProjectPreview>
-                })}
+                <div class='flexWrap'>
+                    {window.bonbonProjects.sort((a, b) => {
+                        return a.order - b.order;
+                    }).map((project, index) => {
+                        return <ProjectPreview 
+                            key={project.id} 
+                            project={project} 
+                            odd={index%2 !== 0}>
+                        </ProjectPreview>
+                    })}
+                </div>
             </div>
         );
     }
--- src/components/projects/ProjectPreview.js
+++ src/components/projects/ProjectPreview.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './ProjectPreview.css';
 
 class ProjectPreview extends Component {
     render() {
         const project = this.props.project;
+        let cssClasses = 'projectPreview';
+        if(this.props.odd) {
+            cssClasses += ' odd';
+        }
         return (
-            <div>
-                <p>{project.title}</p>
-                <img height='350' src='http://via.placeholder.com/350x250'/>
-                <Link to={`/projects/${project.id}`}>voir</Link>
+            <div className={cssClasses}>
+                <div className='container'>
+                    <img height='350' src='http://via.placeholder.com/350x250'/>
+                    <div className='infoBlock'>
+                        <p>{project.title}</p>
+                        <Link to={`/projects/${project.id}`}>voir</Link>
+                    </div>
+                </div>
             </div>
         );
     }
