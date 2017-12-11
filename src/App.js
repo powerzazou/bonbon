553d7da561dcd931186ca2187ab78d4de63dbdbd
superzazou (@home) <vincent.e.dumont@gmail.com>
2017-12-11 22:22:10 +0100

    new parallax system

--- src/App.js
+++ src/App.js
@@ -8,28 +8,42 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-        <BrowserRouter>
-            <div className='app'>
-                <Header/>
-                    <div className='parallax-wrapper'>
-                        <div className='parallax-element'>
-                            <Route path="/" exact={true} component={HomePage}/>
-                            <Route path="/projects/:id" component={ProjectPage}/>
-                            <Footer/>
+    constructor (props) {
+        super(props);
+        this.currentScroll = 0;
+        this.currentParallaxPosition = 0;
+    }
+    handleScroll (e) {
+        // Attention aux perfs la hein !
+        const parallaxSpeed = 0.5;
+        const newScroll = (this.currentScroll - window.scrollY) * parallaxSpeed;
+        this.currentParallaxPosition += newScroll;
+        document.querySelector('.parallax-wrapper').style.backgroundPositionY = this.currentParallaxPosition + 'px';
+        this.currentScroll = window.scrollY;
+    }
+    render () {
+        return (
+            <BrowserRouter>
+                <div className='app'>
+                    <Header/>
+                        <div className='gradient-wrapper'>
+                            <div className='parallax-wrapper'>
+                                <div className='parallax-element'>
+                                    <Route path="/" exact={true} component={HomePage}/>
+                                    <Route path="/projects/:id" component={ProjectPage}/>
+                                </div>
+                            </div>
                         </div>
-                        <div className='parallax-element-after'></div>
-                    </div>
-            </div>
-        </BrowserRouter>
-    );
-  }
+                    <Footer/>
+                </div>
+            </BrowserRouter>
+        );
+    }
 
-  componentDidMount() {
-      const parallaxHeight = document.querySelector('.parallax-element').offsetHeight;
-      document.querySelector('.parallax-element-after').style.height = parallaxHeight + 'px';
-  }
+    componentDidMount() {
+        this.currentScroll = window.scrollY;
+        document.addEventListener('scroll', (e) => this.handleScroll(e))
+    }
 }
 
 export default App;
