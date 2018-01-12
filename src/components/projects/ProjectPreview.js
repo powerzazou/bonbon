f1d33c35bc6ed8b81ceb66967676b623f74260b4
Vincent Dumont <VDumont@voyages-sncf.com>
2018-01-12 17:00:50 +0100

    add project page

--- src/components/projects/ProjectPreview.js
+++ src/components/projects/ProjectPreview.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './ProjectPreview.css';
+import { withRouter } from 'react-router-dom';
+
 
 class ProjectPreview extends Component {
     constructor (props) {
         super(props);
         this.state = {
-            focused: false
+            focusedClass: false
         }
     }
     handleMouseEnter () {
-        this.setState({focused: true});
+        this.setState({focusedClass: 'focused'});
     }
     handleMouseLeave () {
-        this.setState({focused: false});
+        this.setState({focusedClass: 'unfocused'})
+    }
+    handleClick () {
+        this.props.history.push('/projects/' + this.props.project.id);
     }
     render() {
         const project = this.props.project;
@@ -21,11 +26,11 @@ class ProjectPreview extends Component {
         if(this.props.odd) {
             cssClasses += ' odd';
         }
-        const infoBlockClasses = (this.state.focused) ? 'infoBlock focused' : 'infoBlock';
+        const infoBlockClasses = (typeof this.state.focusedClass === 'string') ? 'infoBlock ' + this.state.focusedClass : 'infoBlock';
         // <Link to={`/projects/${project.id}`}>voir</Link>
         return (
             <div className={cssClasses}>
-                <div className='container' onMouseEnter={() => {this.handleMouseEnter()}} onMouseLeave={() => {this.handleMouseLeave()}}>
+                <div className='container' onMouseEnter={() => {this.handleMouseEnter()}} onMouseLeave={() => {this.handleMouseLeave()}} onClick={() => (this.handleClick())}>
                     <img src='http://via.placeholder.com/510x340'/>
                     <div className={infoBlockClasses}>
                         <p className='projectTitle'>{project.title}</p>
@@ -38,4 +43,4 @@ class ProjectPreview extends Component {
     }
 }
 
-export default ProjectPreview;
+export default withRouter(ProjectPreview);
